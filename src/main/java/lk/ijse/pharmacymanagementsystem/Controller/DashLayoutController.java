1e02d5f1282dae425a42464b991c403be3f17cdc
dassana002 <dassana002@gmail.com>
2025-12-06 19:10:46 +0530

    Refactor DashLayoutController and update DashBoard CSS; remove unused styles, enhance button and label styling, and adjust layout properties

--- src/main/java/lk/ijse/pharmacymanagementsystem/Controller/DashLayoutController.java
+++ src/main/java/lk/ijse/pharmacymanagementsystem/Controller/DashLayoutController.java
@@ -3,20 +3,18 @@ package lk.ijse.pharmacymanagementsystem.Controller;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
-import lk.ijse.pharmacymanagementsystem.Launcher;
-
-import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class DashLayoutController implements Initializable {
 
+    @FXML
+    private AnchorPane mainContent;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
+        
     }
 }
 
