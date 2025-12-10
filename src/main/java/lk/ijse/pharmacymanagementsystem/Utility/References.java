944078552c1ba4bf12add43c6eed8e9672b1f99f
dassana002 <dassana002@gmail.com>
2025-12-11 01:00:32 +0530

    Add References utility class for controller management

---
+++ src/main/java/lk/ijse/pharmacymanagementsystem/Utility/References.java
@@ -0,0 +1,11 @@
+package lk.ijse.pharmacymanagementsystem.Utility;
+
+import lk.ijse.pharmacymanagementsystem.controller.DashBoardController;
+import lk.ijse.pharmacymanagementsystem.controller.ItemController;
+import lk.ijse.pharmacymanagementsystem.controller.layout.DashLayoutController;
+
+public class Reference {
+    public static DashBoardController dashBoardController;
+    public static ItemController  itemController;
+    public static DashLayoutController dashLayoutController;
+}
