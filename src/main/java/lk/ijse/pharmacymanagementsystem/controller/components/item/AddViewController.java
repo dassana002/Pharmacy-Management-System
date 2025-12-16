293c6034e9a84bd2d429a9089370b7f29f601f31
dassana002 <dassana002@gmail.com>
2025-12-16 16:35:16 +0530

    date handling and item numbering in the table view

--- src/main/java/lk/ijse/pharmacymanagementsystem/controller/components/item/AddViewController.java
+++ src/main/java/lk/ijse/pharmacymanagementsystem/controller/components/item/AddViewController.java
@@ -3,27 +3,21 @@ package lk.ijse.pharmacymanagementsystem.controller.components.item;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.control.TableColumn;
 import lk.ijse.pharmacymanagementsystem.Dto.AddItemTM;
 
 import java.net.URL;
+import java.time.LocalDate;
 import java.util.ResourceBundle;
 
 public class AddViewController implements Initializable {
 
     @FXML
-    private DatePicker DueDate_text;
+    private DatePicker expireDate_text;
 
     @FXML
-    private DatePicker ExpireDate_text;
-
-    @FXML
-    private DatePicker ReceivedDate_text;
+    private DatePicker receivedDate_text;
 
     @FXML
     private TextField availableQty_txt;
@@ -84,15 +78,52 @@ public class AddViewController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        colNo.setCellValueFactory(new PropertyValueFactory<>("NO"));
+
+        // Auto Number
+        colNo.setCellFactory(column -> new TableCell<AddItemTM, Integer>() {
+            @Override
+            protected void updateItem(Integer item, boolean empty) {
+                super.updateItem(item, empty);
+
+                if (empty) {
+                    setText(null);
+                } else {
+                    setText(String.valueOf(getIndex() + 1));
+                }
+            }
+        });
+
+//        colNo.setCellValueFactory(new PropertyValueFactory<>("NO"));
         colDesc.setCellValueFactory(new PropertyValueFactory<>("Description"));
         colCost.setCellValueFactory(new PropertyValueFactory<>("Unit_Cost"));
         colSellPrice.setCellValueFactory(new PropertyValueFactory<>("Sell_Price"));
         colQty.setCellValueFactory(new PropertyValueFactory<>("QTY"));
         colSubTotal.setCellValueFactory(new PropertyValueFactory<>("Sub_Total"));
+
+        // Today Date Add
+        todayDate_text.setValue(LocalDate.now());
+        receivedDate_text.setValue(LocalDate.now());
+    }
+    
+    @FXML
+    void handleAddToCart(ActionEvent event) {
+        String invoiceText= invoice_number_text.getText();
+        String todayDate = todayDate_text.getValue().toString();
+        String receivedDate = receivedDate_text.getValue().toString();
+        int itemCode = Integer.parseInt(itemCode_text.getText());
+        String dosage = dosage_txt.getText();
+        double unitCost = Double.parseDouble(unitCost_txt.getText());
+        double sellPrice = Double.parseDouble(sellPrice_txt.getText());
+        int qty = Integer.parseInt(qty_txt.getText());
     }
 
-    public void addToCart() {
-        invoice_number_text.getText();
+    @FXML
+    void handleCloseBIll(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleHoldLIst(ActionEvent event) {
+
     }
 }
