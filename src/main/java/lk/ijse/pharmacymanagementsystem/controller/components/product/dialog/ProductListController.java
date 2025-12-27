37c11b0dd6e966818a26eef14185a7e4495ff624
dassana002 <dassana002@gmail.com>
2025-12-27 16:09:27 +0530

    Enhance Product and Supplier dialogs: Update FXML layouts for improved UI, add prompt texts for input fields, and implement scrollable form fields for better usability.

---
+++ src/main/java/lk/ijse/pharmacymanagementsystem/controller/components/product/dialog/ProductListController.java
@@ -0,0 +1,39 @@
+package lk.ijse.pharmacymanagementsystem.controller.components.product.dialog;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+
+import java.awt.event.KeyEvent;
+
+public class ProductListController {
+
+    @FXML
+    private TableColumn<?, ?> des_col;
+
+    @FXML
+    private TableColumn<?, ?> dosage_col;
+
+    @FXML
+    private TextField itemCode_txt;
+
+    @FXML
+    private TableView<?> itemTable;
+
+    @FXML
+    private TableColumn<?, ?> itemCode_col;
+
+    @FXML
+    private StackPane mainContent;
+
+    @FXML
+    private VBox popupCard;
+
+    @FXML
+    void handleFindItem(KeyEvent event) {
+
+    }
+}
